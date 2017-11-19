b51f6c46f333a1fad885eada8922498c27a4391d
Joshua Higham <Josh@higham.com>
2017-11-19 16:05:46 -0700

    Completed Checking if the hand for win states

--- Player.cpp
+++ Player.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -38,4 +40,109 @@ void Player::addCard(Card card) {
 
 PlayerState Player::getState() {
     return state;
+}
+
+
+RankedWin Player::getHighState(std::vector<Card> river) {
+    // Combine the 2 vectors for simple searching
+    std::vector<Card> v(river);
+    for (int i = 0; i < (int)hand.size(); ++i) {
+        v.push_back(hand[i]);
+    }
+
+    // Sort the Deck by smaller Rank, then largest suit
+    std::sort(v.begin(), v.end());
+
+    std::cout << "Sorted: | Size=" << v.size() << std::endl;
+    for (int i = 0; i < (int)v.size(); ++i) {
+        v[i].printCard();
+        std::cout << "\t";
+    }
+
+
+    // Start for the least likely scenerio to the most likely
+    /* Check for Straight Aspect */
+    Suit s = v[0].getSuit();
+    bool isStraight = false, isFlush = true;
+    for (int i = 0; i < (int)v.size() - 4; ++i) {
+        if (v[i].getRank() + 1 == v[i+1].getRank() &&
+            v[i+1].getRank() + 1 == v[i+2].getRank() &&
+            v[i+2].getRank() + 1 == v[i+3].getRank() &&
+            v[i+3].getRank() + 1 == v[i+4].getRank()) {
+            isStraight = true;
+        }
+    }
+
+    /* Check for Flush Aspect */
+    int riverFailed = 0;
+    for (int i = 1; i < (int)v.size(); ++i) {
+        if (v[i].getSuit() != s) {
+            riverFailed++;
+        }
+        if (riverFailed == 3) {
+            isFlush = false;
+            break;
+        }
+    }
+
+    if (isStraight && isFlush) {
+        // ROYAL_FLUSH
+        if (v[v.size()-1].getRank() == ACE) {
+            return ROYAL_FLUSH;
+        }
+
+        // STRAIGHT_FLUSH
+        return STRAIGHT_FLUSH;
+    } else if (isFlush) {
+        // FLUSH
+        return FLUSH;
+    } else if (isStraight) {
+        // STRAIGHT
+        return STRAIGHT;
+    }
+
+    // FOUR_OF_A_KIND
+    for (int i = 0; i < (int)v.size() - 3; ++i) {
+        if (v[i].getRank() == v[i+1].getRank() &&
+            v[i+1].getRank() == v[i+2].getRank() &&
+            v[i+2].getRank() == v[i+3].getRank()) {
+            return FOUR_OF_A_KIND;
+        }
+    }
+
+    /* Three Pair Checking */
+    bool isThree = false;
+    for (int i = 0; i < (int)v.size() - 2; ++i) {
+        if (v[i].getRank() == v[i+1].getRank() &&
+            v[i+1].getRank() == v[i+2].getRank()) {
+            isThree = true;
+        }
+    }
+
+    /* Two Pair Checking */
+    int pairs = 0;
+    for (int i = 0; i < (int)v.size(); ++i) {
+        for (int j = i+1; j < (int)v.size(); ++j) {
+            if (v[i].getRank() == v[j].getRank()) {
+                pairs++;
+            }
+        }
+    }
+
+    if (isThree && pairs > 3) {
+        // FULL_HOUSE
+        return FULL_HOUSE;
+    } else if (isThree) {
+        // THREE_KIND
+        return THREE_KIND;
+    } else if (pairs == 2) {
+        // TWO_PAIR
+        return TWO_PAIR;
+    } else if (pairs == 1) {
+        // PAIR
+        return PAIR;
+    } else {
+        // HIGH_CARD
+        return HIGH_CARD;
+    }
 }
