b4dcbe614ba7f7986bb3512103ce5bc148cd4f75
Joshua Higham <Josh@higham.com>
2017-11-20 00:38:38 -0700

    The red for Hearts was a bit too hard to read

--- Globals.hpp
+++ Globals.hpp
@@ -48,7 +48,7 @@ enum PlayerState {
     BROKE
 };
 
-const std::string red("\033[0;31m");
+const std::string magenta("\033[0;35m");
 const std::string green("\033[1;32m");
 const std::string yellow("\033[1;33m");
 const std::string cyan("\033[0;36m");
